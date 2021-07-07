7c3ad825653f2b4a73d0d6cb7c15132dfe01002f
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-07-07 09:48:54 +0200

    api: add and setup graphql server

--- index.ts
+++ index.ts
@@ -1,7 +1,38 @@
-import { PrismaClient } from '@prisma/client';
+import {customer, PrismaClient } from '@prisma/client';
+import { ApolloServer } from 'apollo-server';
+import * as path from "path";
+import * as fs from "fs";
+
+const db = new PrismaClient();
 
 const doStuff = () => {
-  const db = new PrismaClient();
-
   const bookings = db.booking.findMany({ where: { customerId: 123 } })
 }
+const resolvers = {
+  Query: {
+    getCustomerByEmail: async (parent: string, data: unknown, context: unknown): Promise<customer | null> => {
+      // @ts-ignore
+      const customers = await db.customer.findMany({ where: { email: data.email }});
+      if (customers.length) {
+        return customers[0];
+      }
+      return null
+    },
+  },
+  Mutation: {
+    addCustomer: async (parent: string, data: unknown, context: unknown) => {
+      // @ts-ignore
+      const newCustomer = await db.customer.create({ data });
+      return newCustomer
+    }
+  }
+};
+
+const schemaPath = path.join(__dirname, './graphql/schema.graphql');
+const typeDefs = fs.readFileSync(schemaPath, 'utf8');
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
