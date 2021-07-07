d680816e68d024b28c852a9099a50c50847bde80
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-07-07 17:37:50 +0200

    Updates

---
+++ _next/static/X-dkGPha4x1rZHrWA-9go/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/155-bf6ca4bd5fd2abfa9be6.js","static/css/ae21c9d227d05194e6d2.css","static/chunks/pages/index-d0ad191a3d66cebda1ae.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
